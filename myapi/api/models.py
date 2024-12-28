aaf354bc1a21b7d072ac6d1ce64c8f11bfe1ab64
todd-estes <elestes@gmail.com>
2024-12-28 16:07:31 -0500

    data: Create Template and Notification tables

--- myapi/api/models.py
+++ myapi/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Template(models.Model):
+  body = models.CharField(max_length=100, blank=False)
+
+  def __str__(self):
+    return self.body[:50]
+  
+  class Meta:
+    db_table = 'templates'
+
+class Notification(models.Model):
+  phone_number = models.CharField(max_length=15, blank=False)
+  template = models.ForeignKey(
+    Template,
+    on_delete=models.CASCADE,
+    related_name='notifications',
+    null=False
+  )
+
+  personalization = models.CharField(max_length=50, blank=True, null=True)
+
+  def __str__(self):
+    return f"Notification for {self.phone_number}"
+  
+  class Meta:
+    db_table = 'notifications'
+
--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -47,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api.apps.ApiConfig',
-    'rest_framework',
 ]
 
 ROOT_URLCONF = 'myapi.urls'
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myapi_db',
+        'USER': 'myapi_user',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
