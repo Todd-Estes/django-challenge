ccfa9446b504c320d4b74e5accb2fec9e6097123
todd-estes <elestes@gmail.com>
2024-12-28 22:03:02 -0500

    feat: Create Template endpoints

--- myapi/api/views.py
+++ myapi/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Template
+from .serializers import TemplateSerializer
 
 # Create your views here.
+
+class TemplateViewSet(viewsets.ModelViewSet):
+  queryset = Template.objects.all()
+  serializer_class = TemplateSerializer
